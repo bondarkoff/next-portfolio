65dfbe303cfb565f25a8c59b2c321bd662947f00
bondarkoff <yehor.bondarkoff@gmail.com>
2023-06-07 12:15:50 +0300

    first look home page + yarn add css-shortcut

--- src/pages/index.js
+++ src/pages/index.js
@@ -8,11 +8,20 @@ export default function Home() {
                 <meta name='description' content='Bondarkoff Next Portfolio' />
                 <meta name='viewport' content='width=device-width, initial-scale=1' />
             </Head>
-            <main>
+            <div className=''>
                 <div className='container'>
-                    <div>Hello World</div>
+                    <div className='text'>
+                        <h1 className='title'>Bondarkoff</h1>
+                        <p className='description'>
+                            Welcome to the extraordinary world of , a versatile and imaginative
+                            artist who skillfully navigates the realms of product design,
+                            photography and digital art. With a keen eye for detail and a relentless
+                            pursuit of innovation, crafts captivating visual narratives, immersive
+                            digital realms, and functional yet aesthetically pleasing products.
+                        </p>
+                    </div>
                 </div>
-            </main>
+            </div>
         </>
     );
 }
