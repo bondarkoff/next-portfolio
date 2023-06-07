b3989248f639fda56df253f5d6dd69495097ed10
bondarkoff <yehor.bondarkoff@gmail.com>
2023-06-07 12:09:23 +0300

    added inter font via next/font/google

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,5 +1,12 @@
+import { Inter } from 'next/font/google';
 import '@/styles/globals.scss';
 
+const inter = Inter({ weight: ['400', '500', '700', '800', '900'], subsets: ['latin'] });
+
 export default function App({ Component, pageProps }) {
-    return <Component {...pageProps} />;
+    return (
+        <main className={inter.className}>
+            <Component {...pageProps} />
+        </main>
+    );
 }
