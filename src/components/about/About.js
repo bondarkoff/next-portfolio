9ba646c220529f61fd5963eb1c899bf520a24e41
bondarkoff <yehor.bondarkoff@gmail.com>
2023-06-07 13:12:52 +0300

    stylized about

--- src/components/about/About.js
+++ src/components/about/About.js
@@ -1,16 +1,28 @@
+import Image from 'next/image';
 import styles from './About.module.scss';
 
 export const About = () => {
     return (
-        <div className={styles.text}>
-            <h1 className={styles.title}>Bondarkoff</h1>
-            <p className={styles.description}>
-                Welcome to the extraordinary world of , a versatile and imaginative artist who
-                skillfully navigates the realms of product design, photography and digital art. With
-                a keen eye for detail and a relentless pursuit of innovation, crafts captivating
-                visual narratives, immersive digital realms, and functional yet aesthetically
-                pleasing products.
-            </p>
+        <div className={styles.about}>
+            <div className='text'>
+                <h1 className={styles.title}>Bondarkoff</h1>
+                <p className={styles.description}>
+                    Welcome to the extraordinary world of <span>Bondarkoff</span>, a versatile and
+                    <br></br>
+                    imaginative artist who skillfully navigates the realms of
+                    <span>product design</span>,<br></br>
+                    <span>photography</span> and <span>digital art</span>. With a keen eye for
+                    detail and a relentless
+                    <br></br>pursuit of innovation, <span>Bondarkoff</span> crafts captivating
+                    <span>visual narratives</span>,<br></br>
+                    immersive <span>digital realms</span>, and functional yet
+                    <span>aesthetically pleasing products.</span>
+                </p>
+            </div>
+            <div className='pos-r'>
+                <Image src={'/avatar.png'} alt='Avatar Image' width={350} height={350} priority />
+                <div className={styles.blur}></div>
+            </div>
         </div>
     );
 };
--- src/pages/_app.js
+++ src/pages/_app.js
@@ -3,7 +3,7 @@ import 'css-shortcut';
 
 import '@/styles/globals.scss';
 
-const inter = Inter({ weight: ['400', '500', '700', '800', '900'], subsets: ['latin'] });
+const inter = Inter({ weight: ['200', '400', '500', '700', '800', '900'], subsets: ['latin'] });
 
 export default function App({ Component, pageProps }) {
     return (
